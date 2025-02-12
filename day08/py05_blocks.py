135fe3f86b178f19c221188169c134afe14a5417
suuurin <z11673@naver.com>
2025-02-12 17:12:09 +0900

    블록게임 완료

--- day08/py05_blocks.py
+++ day08/py05_blocks.py
@@ -14,7 +14,8 @@ class Block:
         self.col = col
         self.rect = rect
         self.speed = speed
-        self.dir = random.randint(-45, 45) + 270    # 225 ~ 315
+        # 90이면 위로, 270이면 공이 아래로 -45~45편차로
+        self.dir = random.randint(-45, 45) + 90    
 
     def move(self): # 볼 무브
         # 볼이 움직이는 x축 값을 계속 계산하려면 x은 dir 값을 라디언으로 변환 후 코사인처리
@@ -125,7 +126,8 @@ def main():
             if BALL.rect.centery > 800:
                 Surface.blit(M_FAIL, ((SCREEN_WIDTH / 2) - (240 / 2)
                                     ,(SCREEN_HEIGHT / 2) - (50 / 2)))
-                # is_game_start = False # 게임 종료 후 재시작은 나중에 다시!
+                is_game_start = False # 게임 종료 후 재시작
+                BALL = Block((200,200,0), Rect(375,650,20,20), 10) # 공을 새로 생성
 
             BALL.draw_E()
             PADDLE.draw_R()
